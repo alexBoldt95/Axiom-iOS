18c3ee21b843d3ea00d268123182fc7adf7d34a5
Alex Boldt <alexboldt95@gmail.com>
2025-02-08 12:51:26 -0600

    Initial Commit

---
+++ Axiom-iOS/Axiom_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Axiom_iOSApp.swift
+//  Axiom-iOS
+//
+//  Created by Alexander Boldt on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Axiom_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Axiom-iOSTests/Axiom_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Axiom_iOSTests.swift
+//  Axiom-iOSTests
+//
+//  Created by Alexander Boldt on 2/8/25.
+//
+
+import Testing
+@testable import Axiom_iOS
+
+struct Axiom_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
